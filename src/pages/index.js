4917320b748611a4a51b216725fdfd2f53bca24a
Miron Banks <madapegarage@gmail.com>
2020-02-13 02:11:16 +0100

    checking styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const StyledDiv = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+`
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +18,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <StyledDiv />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
